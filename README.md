9dc30be829eaf1cc1d84e5a3004fb913bb7deab0
FZQ0003 <468403876@qq.com>
2024-10-10 20:43:47 +0800

    chore: Update demo.py & README.md

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# avilla-lagrange
+# Avilla-Lagrange
 
 该项目仅作为可行性验证：利用[Avilla](https://github.com/GraiaProject/Avilla)的抽象能力，对接[另一个bot框架](https://github.com/LagrangeDev/lagrange-python)，使两个（或多个）框架集成于同一程序中。
 
@@ -9,3 +9,8 @@
 ## 环境搭建
 
 可以直接使用PDM等工具搭建，也可以参考[这个脚本](https://github.com/FZQ0003/Qi-Bot/blob/avilla/venv_script_avilla.sh)，搭建虚拟环境后把项目以软链接的形式丢进`site-packages`。
+
+## 已知问题
+
+* 目前尚未实现`lagrange-python`提供的所有功能。
+* 当前函数重载方式需要在`Avilla`使用`flywheel`重构功能后同步更新。
--- demo.py
+++ demo.py
@@ -1,6 +1,9 @@
 import os
 
 from avilla.core import Avilla, Context, MessageReceived
+from lagrange import Client as LgrClient
+from lagrange.client.events import BaseEvent
+from loguru import logger
 
 from avilla.lagrange.const import SIGN_URL
 from avilla.lagrange.protocol import LagrangeConfig, LagrangeGlobalConfig, LagrangeProtocol
@@ -18,8 +21,12 @@ global_config = LagrangeGlobalConfig('demo-database.db')
 avilla.apply_protocols(LagrangeProtocol(global_config).configure(config))
 
 
+# Note: raw event & client from lagrange-python can be collected here.
 @avilla.listen(MessageReceived)
-async def on_message_received(ctx: Context, event: MessageReceived):
+async def on_message_received(ctx: Context, event: MessageReceived,
+                              raw_event: BaseEvent, raw_client: LgrClient):
+    logger.debug(f'Raw: [{raw_client.uin}] -> {raw_event}')
+    print(Avilla.current())
     await ctx.scene.send_message('Hello, Avilla!', reply=event.message)
 
 avilla.launch()
