f3fe09300ab46558bd316fe640e649980a4ccc97
FZQ0003 <468403876@qq.com>
2024-11-08 01:59:34 +0800

    chore: FN_MAP (WIP)

--- src/avilla/lagrange/perform/compat/__init__.py
+++ src/avilla/lagrange/perform/compat/__init__.py
@@ -2,13 +2,19 @@ from typing import ClassVar, TYPE_CHECKING
 
 from avilla.core import Avilla, Context
 from avilla.core.ryanvk.collector.base import AvillaBaseCollector
-from graia.ryanvk import Access, OptionalAccess
+from graia.ryanvk import Access, OptionalAccess, Fn, Capability
+from flywheel import SimpleOverload
+from flywheel_extras import FnCollector
+from flywheel_extras.utils import get_var_names
 from lagrange import Client
 
+from .overload import convert_overload
+from ...utils.misc import get_subclasses
+
 if TYPE_CHECKING:
-    from ..client import LagrangeClientService
-    from ..protocol import LagrangeProtocol
-    from ..service import LagrangeDatabase
+    from ...client import LagrangeClientService
+    from ...protocol import LagrangeProtocol
+    from ...service import LagrangeDatabase
 
 
 class LagrangeClientCollector(AvillaBaseCollector):
@@ -32,8 +38,21 @@ class LagrangeClientCollector(AvillaBaseCollector):
         return PerformTemplate
 
 
+FN_MAP: dict[Fn, FnCollector] = {
+    _f: FnCollector(
+        base=_f.shape,
+        overloads={
+            _n: (convert_overload(_o, _n), SimpleOverload('fallback@' + _n))
+            for _n, _o in _f.overload_params.items() if _n in get_var_names(_f.shape)
+        }
+    )
+    for _c in get_subclasses(Capability, False) for _f in _c.__dict__.values() if isinstance(_f, Fn)
+}
+
+
 class LagrangeCompatPerform((m := LagrangeClientCollector())._):
     m.namespace = 'avilla.protocol/lagrange::compat'
+    # TODO: All TargetOverload; MetadataOverload?
     # from avilla.core.builtins.capability import CoreCapability
     # from avilla.standard.core.activity.capability import ActivityTrigger
     # from avilla.standard.core.message.capability import (
