02bfab514d8de1be61513f7dfea98134e2799fef
FZQ0003 <468403876@qq.com>
2024-11-09 01:06:33 +0800

    chore: Format

--- test_lagrange.py
+++ test_lagrange.py
@@ -2,10 +2,12 @@ import os
 
 from lagrange import Lagrange, install_loguru
 
+from avilla.lagrange.const import SIGN_URL
+
 lag = Lagrange(
     int(os.environ.get('LAGRANGE_UIN', '0')),
     'linux',
-    os.environ.get('LAGRANGE_SIGN_URL', '')
+    os.environ.get('LAGRANGE_SIGN_URL', SIGN_URL)
 )
 install_loguru()
 lag.log.set_level('DEBUG')
